af68366e01bf2a9425a9e326c3e82b72de7be6a6
Nicolás Venturo <nicolas.venturo@gmail.com>
2018-01-24 22:58:31 -0300

    Added functions

--- src/egg.js
+++ src/egg.js
@@ -114,6 +114,27 @@ specialForms["define"] = function(args, env) {
   return value;
 };
 
+specialForms["fun"] = function(args, env) {
+  if (!args.length)
+    throw new SyntaxError("Functions need a body");
+  function name(expr) {
+    if (expr.type != "word")
+      throw new SyntaxError("Arg names must be words");
+    return expr.name;
+  }
+  var argNames = args.slice(0, args.length - 1).map(name);
+  var body = args[args.length - 1];
+
+  return function() {
+    if (arguments.length != argNames.length)
+      throw new TypeError("Wrong number of arguments");
+    var localEnv = Object.create(env);
+    for (var i = 0; i < arguments.length; i++)
+      localEnv[argNames[i]] = arguments[i];
+    return evaluate(body, localEnv);
+  };
+};
+
 // Runtime
 
 var topEnv = Object.create(null);
