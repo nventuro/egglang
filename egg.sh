45ce29bac14b8e9deadd5cf6a7cfbb9e40c9453a
Nicolás Venturo <nicolas.venturo@gmail.com>
2018-01-25 02:48:19 -0300

    Changed executable to support REPL.

--- egg.sh
+++ egg.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-node src/egg.js "$(cat $1)"
+node src/egg.js $@
--- src/egg.js
+++ src/egg.js
@@ -1,12 +1,18 @@
 const runtime = require("./runtime.js");
+const fs = require("fs");
 
 if (require.main === module) {
   var args = process.argv.slice(2); // Remove the Node call to this program from argv
 
-  if (args.length != 1) {
-    throw new Error("Wrong number of arguments");
-  }
+  if (args.length == 0) {
+    throw new Error("No REPL yet");
 
-  var program = args[0];
-  runtime.run(program);
+  } else if (args.length == 1) {
+    var filename = args[0];
+    var program = fs.readFileSync(filename, "utf8");
+    runtime.run(program);
+
+  } else {
+    throw new Error("Usage: egg.sh [filename]");
+  }
 }
