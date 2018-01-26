c81bd78f410ef2572ddc77e88fab4423e9fcb9d1
Nicolás Venturo <nicolas.venturo@gmail.com>
2018-01-25 22:51:39 -0300

    Improved parser tests

--- test/parser.js
+++ test/parser.js
@@ -3,25 +3,38 @@ const expect = require("chai").expect;
 
 describe("Ignored characters remover", () => {
   describe("Whitespace remover", () => {
-    it("removes whitespapce", () => {
-      expect(parser._removeIgnored("   ")).to.deep.equal(""); // Remove empty lines
-      expect(parser._removeIgnored("   abc")).to.deep.equal("abc"); // Remove leading
-      expect(parser._removeIgnored("abc   ")).to.deep.equal("abc"); // Remove trailing
-      expect(parser._removeIgnored("ab    c")).to.deep.equal("abc"); // Remove in the middle
-
-      expect(parser._removeIgnored("ab\nc")).to.deep.equal("abc"); // Remove newlines
-
-      expect(parser._removeIgnored("\n   ab \n  c   \n")).to.deep.equal("abc"); // Remove all combined
+    it("removes empty lines", () => {
+      expect(parser._removeIgnored("   ")).to.deep.equal("");
+    });
+    it("removes leading whitespace", () => {
+      expect(parser._removeIgnored("   abc")).to.deep.equal("abc");
+    });
+    it("removes trailing whitespace", () => {
+      expect(parser._removeIgnored("abc   ")).to.deep.equal("abc");
+    });
+    it("removes middle whitespace", () => {
+      expect(parser._removeIgnored("ab    c")).to.deep.equal("abc");
+    });
+    it("removes newlines", () => {
+      expect(parser._removeIgnored("ab\nc")).to.deep.equal("abc");
+    });
+    it("removes all whitespace characters", () => {
+      expect(parser._removeIgnored("\n   ab \n  c   \n")).to.deep.equal("abc");
     });
   });
 
   describe("Comments remover", () => {
-    it("removes comments", () => {
-      expect(parser._removeIgnored("abc#comment")).to.deep.equal("abc"); // Remove at end
-      expect(parser._removeIgnored("abc#comment#abc")).to.deep.equal("abc"); // Remove nested comments
-
-      expect(parser._removeIgnored("#comment")).to.deep.equal(""); // Remove comment lines
-      expect(parser._removeIgnored("#comment1\n#comment2")).to.deep.equal(""); // Remove multiline comments
+    it("removes trailing comments", () => {
+      expect(parser._removeIgnored("abc#comment")).to.deep.equal("abc");
+    });
+    it("removes nested comments", () => {
+      expect(parser._removeIgnored("abc#comment#abc")).to.deep.equal("abc");
+    });
+    it("removes comment lines", () => {
+      expect(parser._removeIgnored("#comment")).to.deep.equal("");
+    });
+    it("removes multiline comments", () => {
+      expect(parser._removeIgnored("#comment1\n#comment2")).to.deep.equal("");
     });
   });
 });
