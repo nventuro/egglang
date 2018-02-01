3eafb4eafd3c401c4e52c297d470abbce4af4b29
Nicolás Venturo <nicolas.venturo@gmail.com>
2018-02-01 04:03:13 -0300

    Added modules sample.

--- test/samples.js
+++ test/samples.js
@@ -9,24 +9,6 @@ describe("Samples", () => {
     envNoPrint["print"] = (val) => val;
   });
 
-  describe("Array sample", () => {
-    it("should return the correct result", () => {
-      expect(egg._runFile("samples/array.egg", envNoPrint)).to.deep.equal(55);
-    });
-  });
-
-  describe("Dict sample", () => {
-    it("should return the correct result", () => {
-      expect(egg._runFile("samples/dict.egg", envNoPrint)).to.deep.equal({"even": [0, 2, 4, 6, 8], "odd": [1, 3, 5, 7, 9]});
-    });
-  });
-
-  describe("Closure sample", () => {
-    it("should return the correct result", () => {
-      expect(egg._runFile("samples/closure.egg", envNoPrint)).to.deep.equal(8);
-    });
-  });
-
   describe("Digit sum sample", () => {
     it("should return the correct result", () => {
       expect(egg._runFile("samples/digit_sum.egg", envNoPrint)).to.deep.equal(55);
@@ -42,6 +24,30 @@ describe("Samples", () => {
   describe("Power sample", () => {
     it("should return the correct result", () => {
       expect(egg._runFile("samples/pow.egg", envNoPrint)).to.deep.equal(1024);
+    });
+  });
+
+  describe("Closure sample", () => {
+    it("should return the correct result", () => {
+      expect(egg._runFile("samples/closure.egg", envNoPrint)).to.deep.equal(8);
+    });
+  });
+
+  describe("Array sample", () => {
+    it("should return the correct result", () => {
+      expect(egg._runFile("samples/array.egg", envNoPrint)).to.deep.equal(55);
+    });
+  });
+
+  describe("Dict sample", () => {
+    it("should return the correct result", () => {
+      expect(egg._runFile("samples/dict.egg", envNoPrint)).to.deep.equal({"even": [0, 2, 4, 6, 8], "odd": [1, 3, 5, 7, 9]});
+    });
+  });
+
+  describe("Modules sample", () => {
+    it("should return the correct result", () => {
+      expect(egg._runFile("samples/modules.egg", envNoPrint)).to.deep.equal(40);
     });
   });
 });
